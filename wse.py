cd08d190effdfa2a283443289dbaccf3b1192530
Altair <maciejkamat@gmail.com>
2016-08-28 16:05:01 +0200

    incomplete changes
    still has to finish and test everything
    coming soon.

--- wse.py
+++ wse.py
@@ -7,50 +7,37 @@ import sys
 import json
 import requests
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 def eprint(*args, **kwargs):
 	print(*args, file=sys.stderr, **kwargs)
 
 
-if len(sys.argv) != 3:
-	eprint("Usage:\n./wse.py [json config] [db file]")
+if len(sys.argv) != 2:
+	eprint("Usage:\n./wse.py [json config]")
 	sys.exit()
 
-#meh todo
-url="TODO"
-what="TODO"
-search="TODO"
 
-tab = []
-with open("list.txt") as f:
-	tab = f.read().splitlines()
-tab = set(tab)
+with open(sys.argv[1], "r") as config
+	tmpdec = json.loads(config.read())
+	pprint(tmpdec)
+	url = tmpdec["url"] #page url
+	domn = tmpdec["element"] #attr of a specific tag
+	search = tmpdec["regex"] #extract info from attr
+	dbfile = tmpdec["output"] #where to output
+	unique = tmpdec["unique"] #todo
+	delay = tmpdec["delay"] #delay in ms
 
-rld = open("list.txt", "w")
-
-for item in tab:
-	rld.write("%s\n" % item)
-	
-rld.close()
-
-file = open("list.txt", "a")
-log = open("log.txt", "w")
-
+res = open(dbfile, "a")
 
 t0 = time.clock()
-item_count = 9999999999
 while len(tab)<item_count:
 	page = requests.get(url)
 	soup = BeautifulSoup(page.content, "html.parser")
-	item_count = int(re.search(search, str(soup.title)).group(1))
-	cod = re.search(what, soup.source.get('src')).group(1)
-	if not (cod in tab):
-		tab.add(cod)
-		file.write(cod+"\n")
-		print(str(len(tab))+"/"+str(item_count) + "\t" + str(time.clock() - t0))
-		log.write(str(len(tab)) + "\t" + str(time.clock() - t0)+"\n")
-		file.flush()
-		log.flush()
-		os.fsync(file.fileno())
-		os.fsync(log.fileno())
-		t0 = time.clock()
+	cod = re.search(regex, eval("soup."+domn)) #ugly eval
+	res.write(cod+"\n")
+	eprint(str(time.clock() - t0)+"\n")
+	res.flush()
+	os.fsync(file.fileno())
+	time.sleep(delay/1000)
+	t0 = time.clock()
