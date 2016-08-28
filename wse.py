7c1503d3d1f06b2a8949ee32e460e09e68e0bd9e
Altair <maciejkamat@gmail.com>
2016-08-28 23:44:18 +0200

    got it working
    kinda works
    has config
    no error handling
    random user-agent
    good for now I guess

--- wse.py
+++ wse.py
@@ -4,13 +4,21 @@ import re
 import time
 import os
 import sys
+import pprint
 import json
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
+#setup
+ua = UserAgent()
+
+pp = pprint.PrettyPrinter(indent=4)
+
 def eprint(*args, **kwargs):
 	print(*args, file=sys.stderr, **kwargs)
+
+#end of setup
 
 
 if len(sys.argv) != 2:
@@ -18,9 +26,9 @@ if len(sys.argv) != 2:
 	sys.exit()
 
 
-with open(sys.argv[1], "r") as config
+with open(sys.argv[1], "r") as config:
 	tmpdec = json.loads(config.read())
-	pprint(tmpdec)
+	pp.pprint(tmpdec)
 	url = tmpdec["url"] #page url
 	domn = tmpdec["element"] #attr of a specific tag
 	search = tmpdec["regex"] #extract info from attr
@@ -30,14 +38,17 @@ with open(sys.argv[1], "r") as config
 
 res = open(dbfile, "a")
 
+header = {'User-Agent': ua.random}
+
 t0 = time.clock()
-while len(tab)<item_count:
-	page = requests.get(url)
+while True: #todo add goal and handler maybe
+	page = requests.get(url, headers=header)
 	soup = BeautifulSoup(page.content, "html.parser")
-	cod = re.search(regex, eval("soup."+domn)) #ugly eval
+	cod = re.search(search, eval("soup."+domn)).group(1) #ugly eval ugh
+	eprint(cod, end="\t")
+	eprint(str(time.clock() - t0))
 	res.write(cod+"\n")
-	eprint(str(time.clock() - t0)+"\n")
 	res.flush()
-	os.fsync(file.fileno())
+	os.fsync(res.fileno())
 	time.sleep(delay/1000)
 	t0 = time.clock()
